234557cc916c0ec50292e73354e613ccb892ce58
IvanHrIvanov <vinko96@abv.bg>
2020-11-19 18:04:45 +0200

    Add

---
+++ 04. Text Filter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] bannedWords = Console.ReadLine().Split(", ");
+
+            string text = Console.ReadLine();
+
+            foreach (var item in bannedWords)
+            {
+                string replace = new string('*',item.Length);
+
+                text = text.Replace(item, replace);
+
+            }
+
+            Console.WriteLine(text);
+
+        }
+    }
+}
